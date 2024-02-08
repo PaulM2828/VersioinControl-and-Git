298a4362b7413b5d6103e4b3b38834ccf4659179
PaulM2828 <159465027+PaulM2828@users.noreply.github.com>
2024-02-08 17:35:43 +0000

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,10 @@
+# wildBearProject.py
+
+# Collect information about a wild Romanian bear
+bear_location = input("Enter the location where the wild bear was spotted: ")
+bear_descriptions = input("Describe the wild bear's appearance and behavior: ")
+
+# Display the gathered information
+print("\nWild Bear Information:")
+print("Location: ", bear_location)
+print("Description: ", bear_descriptions)
